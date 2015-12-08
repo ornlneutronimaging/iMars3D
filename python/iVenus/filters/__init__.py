2b29129e5b50e35df92159da2597187bc7a2d72d
Jiao Lin <linjiao@caltech.edu>
2015-12-08 13:46:29 -0800

    fixed a bug in remove_outliers_bymedian (negative boundaries)

--- python/iVenus/filters/__init__.py
+++ python/iVenus/filters/__init__.py
@@ -3,7 +3,8 @@ def remove_outliers_bymedian(img, outlier_indexes, boxsize=5):
     img[outlier_indexes] = np.nan
     indexes = np.where(img!=img)
     for ind1, ind2 in zip(*indexes):
-        img[ind1, ind2] = np.nanmedian(img[ind1-boxsize:ind1+boxsize, ind2-boxsize:ind2+boxsize])
+        img[ind1, ind2] = np.nanmedian(
+            img[max(ind1-boxsize, 0):ind1+boxsize, max(ind2-boxsize, 0):ind2+boxsize])
         continue
     return img
     
