14b7956714f623f7ba476ab32267d28ad1817f80
Jiao Lin <linjiao@caltech.edu>
2015-12-09 06:54:44 -0800

    trivial: a bit of doc in tests/iVenus/filters/remove_outliers.py

--- tests/iVenus/filters/remove_outliers.py
+++ tests/iVenus/filters/remove_outliers.py
@@ -8,9 +8,11 @@ from iVenus import sim
 img = sim.randomBG(100, 120, 100, 10);
 sim.addRandomOutliers(img, 100, 1000, 10)
 
+# filter
 threshold = 450
 filters.gamma_filtering.remove_outliers_bymedian(img, img>threshold)
 
+# display
 pylab.imshow(img)
 pylab.colorbar()
 pylab.show()
