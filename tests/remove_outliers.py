ec62fb36c0cbf4bceea038ef23859f33a178c118
JeanBilheux <bilheuxjm@ornl.gov>
2015-12-09 09:22:58 -0500

    Changed the way the module is accessed

--- tests/remove_outliers.py
+++ tests/remove_outliers.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
 import numpy as np, pylab
+from iVenus import filters
 
 # create an image with outliers
 from iVenus import sim
 img = sim.randomBG(100, 120, 100, 10);
 sim.addRandomOutliers(img, 100, 1000, 10)
 
-#
-from iVenus import filters
 threshold = 450
-filters.remove_outliers_bymedian(img, img > threshold)
+filters.gamma_filtering.remove_outliers_bymedian(img, img>threshold)
 
 pylab.imshow(img)
 pylab.colorbar()
