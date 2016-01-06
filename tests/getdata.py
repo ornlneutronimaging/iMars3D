f468ab045decdf573fd6f2572cf423da5f0edddf
Jiao Lin <linjiao@ornl.gov>
2016-01-06 15:30:26 -0500

    added tests/getdata.py script to retrieve data into the specific location in tests subdir

---
+++ tests/getdata.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# this script should be run root of iVenus pkg
+
+path = "tests/iVenus_data_set"
+
+import os
+if not os.path.exists(path):
+    cmd = "builders/download-testdata.py %s" % path
+    if os.system(cmd):
+        raise "%s failed" % cmd
