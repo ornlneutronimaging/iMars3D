359ee84e30e42f428c18844990b95d488accde1d
Jiao Lin <jiao.lin@gmail.com>
2016-01-26 14:41:25 -0500

    tilt: try getting shift from peak comparison.

---
+++ tests/ivenus/filters/tilt_verify.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from tilt_inputimages import f0, f180
+from scipy import ndimage
+import numpy as np
+
+rotation = 1.6
+
+data0 = f0.getData()
+data0 = ndimage.rotate(data0, rotation)
+
+data180 = f180.getData()
+data180 = np.fliplr(data180)
+
+import pylab
+# pylab.imshow(data0)
+pylab.imshow(data180)
+pylab.colorbar()
+pylab.show()
