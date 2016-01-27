854e321d5a8cb420da82d2316449aac4038a6e66
Jiao Lin <jiao.lin@gmail.com>
2016-01-27 16:14:06 -0500

    ivenus.tilt: added a method "check"

--- python/ivenus/tilt/__init__.py
+++ python/ivenus/tilt/__init__.py
@@ -1,2 +1,22 @@
 # ivenus.tilt
 
+def check(tilt, img0, img180):
+    """check tilt using a pair of images
+    """
+    from scipy import ndimage
+    import numpy as np
+    
+    data0 = img0.getData()
+    data0 = ndimage.rotate(data0, -tilt)
+    
+    data180 = img180.getData()
+    data180 = ndimage.rotate(data180, -tilt)
+    data180 = np.fliplr(data180)
+    
+    from ivenus import io
+    out = io.ImageFile("tilted-0.npy")
+    out.data = data0; out.save()
+    
+    out = io.ImageFile("tilted-180.npy")
+    out.data = data180; out.save()
+    return
