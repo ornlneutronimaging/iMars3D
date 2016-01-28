73e1f112190a6ba78870c8e6b3d74b4bab31ee59
Jiao Lin <jiao.lin@gmail.com>
2016-01-28 10:04:05 -0500

    ivenus.tilt: added method "apply"

--- python/ivenus/tilt/__init__.py
+++ python/ivenus/tilt/__init__.py
@@ -20,3 +20,16 @@ def check(tilt, img0, img180):
     out = io.ImageFile("tilted-180.npy")
     out.data = data180; out.save()
     return
+
+
+def apply(tilt, img, outimg):
+    """apply tilt to the given image
+    """
+    from scipy import ndimage
+    import numpy as np
+    
+    data = img.getData()
+    data = ndimage.rotate(data, -tilt)
+    outimg.data = data
+    outimg.save()
+    return
