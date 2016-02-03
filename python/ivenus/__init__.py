17a46135720df3c1e26736619bb1fbc43b48d1bd
Jiao Lin <linjiao@ornl.gov>
2016-02-03 14:37:44 -0500

    python/ivenus/__init__.py: load logging config

--- python/ivenus/__init__.py
+++ python/ivenus/__init__.py
@@ -1,2 +1,12 @@
 # iVenus python package
 
+import yaml, os
+conf_path = "ivenus.conf"
+config = dict()
+if os.path.exists(conf_path):
+    config = yaml.load(open(conf_path))
+
+import logging.config
+logging_conf = config.get("logging")
+if logging_conf:
+    logging.config.dictConfig(logging_conf)
