aa2ad1f430c7ce001ceaeb59066d266c07089516
Jiao Lin <linjiao@ornl.gov>
2016-03-10 08:47:38 -0500

    test_imagefileseries.py: added test_iter

--- tests/imars3d/io/test_imagefileseries.py
+++ tests/imars3d/io/test_imagefileseries.py
@@ -2,15 +2,14 @@
 
 import os, numpy as np
 from imars3d import io
+dir = os.path.dirname(__file__)
 
 def test():
-    dir = os.path.dirname(__file__)
     datadir = os.path.join(dir, "../../iMars3D_data_set/turbine")
     ct_series = io.ImageFileSeries(
         os.path.join(datadir, "*CT*_%.3f_*.fits"),
         identifiers = np.arange(0, 52, 8.5),
     )
-
     assert (os.path.basename(ct_series.getFilename(0)) == "20120618_TURBINECT_0180_0_000_0000.fits")
     img = ct_series.getImage(0)
     data = img.getData()
@@ -18,8 +17,19 @@ def test():
     return
 
 
+def test_iter():
+    datadir = os.path.join(dir, "../../iMars3D_data_set/turbine")
+    ct_series = io.ImageFileSeries(
+        os.path.join(datadir, "*CT*_%.3f_*.fits"),
+        identifiers = np.arange(0, 52, 8.5),
+    )
+    for img in ct_series:
+        print(img)
+    return
+
 def main():
     test()
+    test_iter()
     return
 
 
