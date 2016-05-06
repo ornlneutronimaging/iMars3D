27e9d254b69e28030411e105759ca1dec4cd2fef
Jiao Lin <jiao.lin@gmail.com>
2016-05-06 11:16:14 -0400

    Refs #18. adding method "sniff". right now only OB and DF

--- python/imars3d/CT.py
+++ python/imars3d/CT.py
@@ -1,13 +1,56 @@
 # CT data object
 # A CT object contains a CT scan and OB and DF images.
 
+import os, glob
+
 class CT:
 
-    def __init__(self, path):
+    def __init__(self, path,):
         self.path = path
         return
 
     def sniff(self):
+        self.find_OB()
+        self.find_DF()
+        return
+
+    
+    def find_OB(self):
+        return self._find_pattern(
+            subdir_candidates = ['ob', 'OB'],
+            filenamepattern_candidates = ['*ob*', '*OB*'],
+            )
+
+    def find_DF(self):
+        return self._find_pattern(
+            subdir_candidates = ['df', 'DF'],
+            filenamepattern_candidates = ['*df*', '*DF*'],
+            )
+
+
+    def _find_pattern(self, subdir_candidates, filenamepattern_candidates):
+        candidates = subdir_candidates
+        found = None
+        for c in candidates:
+            p = os.path.join(self.path, c)
+            if os.path.exists(c):
+                found = c; break
+            continue
+        if not found:
+            raise IOError(
+                "failed to find OB sub-directory. Candidates: %s" % (
+                    candidates,)
+                )
+        self.OB_subdir = found
+        candidates = filenamepattern_candidates
+        found = None
+        for c in candidates:
+            pattern = os.path.join(self.path, self.OB_subdir, c)
+            files = glob.glob(pattern)
+            if files:
+                found = pattern; break
+            continue
+        self.OB_pattern = found
         return
 
     pass
