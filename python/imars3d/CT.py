4fe5fb3e4cdcf2f6142b1fb8a1d480c33eeadb13
Jiao Lin <linjiao@ornl.gov>
2016-05-06 10:59:55 -0400

    Refs #18. started CT.py

---
+++ python/imars3d/CT.py
@@ -0,0 +1,16 @@
+# CT data object
+# A CT object contains a CT scan and OB and DF images.
+
+class CT:
+
+    def __init__(self, path):
+        self.path = path
+        return
+
+    def sniff(self):
+        return
+
+    pass
+
+
+# End of file
