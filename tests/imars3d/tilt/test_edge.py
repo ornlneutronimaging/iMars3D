8e2574a4ab08c5801efbce4f37a7637f5ebe4a03
Jiao Lin <linjiao@ornl.gov>
2016-07-18 09:36:13 -0400

    Refs #48. experimenting with edge detection

---
+++ tests/imars3d/tilt/test_edge.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os, numpy as np
+
+def test():
+    from skimage import feature
+    from imars3d import io
+    dir = os.path.dirname(__file__)
+    datadir = os.path.join(dir, "..", "..", 'iMars3D_data_set')
+    imgpath = os.path.join(datadir, 'injectorG_0.tiff')
+    img = io.ImageFile(imgpath).data
+    edge = feature.canny(img, sigma=3)
+    edge = np.array(edge, dtype="float32")
+    edgeimg = io.ImageFile(path="edge_sigma3.tiff")
+    edgeimg.data = edge
+    edgeimg.save()
+    return
+
+if __name__ == '__main__': test()
