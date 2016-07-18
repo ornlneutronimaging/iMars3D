18edcb74d712691f139428455e5b4819b43be5ac
Jiao Lin <linjiao@ornl.gov>
2016-07-18 14:21:04 -0400

    Refs #48. trying to match API of use_centers to phasecorrelation

--- python/imars3d/tilt/use_centers.py
+++ python/imars3d/tilt/use_centers.py
@@ -4,10 +4,20 @@ import os, numpy as np
 from imars3d import io
 from matplotlib import pyplot as plt
 
-def computeTilt(img0path, img180path, workdir=None, **kwds):
+class Calculator:
+
+    def __init__(self, logging_dir=None, **opts):
+        self.logging_dir = logging_dir
+        self.opts = opts
+        return
+    
+    def __call__(self, img0, img180):
+        return computeTilt(img0, img180, workdir=self.logging_dir, **self.opts), 1.0
+
+def computeTilt(img0, img180, workdir=None, **kwds):
     centers = np.array(
         [item for item 
-         in iterCenters(img0path, img180path, workdir=workdir, **kwds)])
+         in iterCenters(img0, img180, workdir=workdir, **kwds)])
     rows, centers = centers.T
     cm = np.median(centers)
     csigma = np.std(centers)
@@ -16,27 +26,33 @@ def computeTilt(img0path, img180path, workdir=None, **kwds):
     rows1 = rows[w]
     centers1 = centers[w]
     plt.figure()
+    plt.plot(rows, centers)
+    plt.savefig(os.path.join(workdir, "centers.png"))
+    plt.close()
+    #
+    from scipy import stats
+    slope, intercept, r, p, std_err = stats.linregress(rows1, centers1)
+    plt.figure()
     plt.plot(rows1, centers1)
+    plt.plot(rows1, rows1*slope+intercept, '-')
     plt.savefig(os.path.join(workdir, "linefit.png"))
     plt.close()
-    from scipy import stats
-    slope, intercept, r, p, std_err = stats.linregress(rows, centers)
     print(slope, intercept)
     return
 
 
-def iterCenters(img0path, img180path, workdir=None, sigma=3, maxshift=20):
-    edge0 = getEdge(img0path, os.path.join(workdir, 'edge0.tiff'), sigma=sigma)
-    edge180 = getEdge(img180path, os.path.join(workdir, 'edge180.tiff'), sigma=sigma)
+def iterCenters(img0, img180, workdir=None, sigma=3, maxshift=20):
+    edge0 = getEdge(img0.data, os.path.join(workdir, 'edge0.tiff'), sigma=sigma)
+    edge180 = getEdge(img180.data, os.path.join(workdir, 'edge180.tiff'), sigma=sigma)
     edge180 = edge180[:, ::-1]
     for i, (line0, line180) in enumerate(zip(edge0, edge180)):
         c = _computeCenterOfRot(line0, line180, maxshift=maxshift)
-        yield i, c
+        if c>(line0.size-maxshift)/2.+maxshift//40.: # remove edge cases
+            yield i, c
         continue
     return
 
-def getEdge(imgpath, edgepath, **kwds):
-    img = io.ImageFile(imgpath).data
+def getEdge(img, edgepath, **kwds):
     from skimage import feature
     edge = feature.canny(img, **kwds)
     edge = np.array(edge, dtype="float32")
@@ -56,16 +72,16 @@ def _computeShift(x1, x2, maxshift=20):
     diffs = []
     for dx in range(1-maxshift, maxshift):
         if dx > 0:
-            diff = x1[dx:] - x2[:-dx]
+            diff = x1[dx:] * x2[:-dx]
         elif dx < 0:
-            diff = x1[:dx] - x2[-dx:]
+            diff = x1[:dx] * x2[-dx:]
         else:
-            diff = x1-x2
+            diff = x1*x2
         diff = np.sum(diff*diff)
         diffs.append((dx, diff))
     diffs = np.array(diffs)
     # np.save("diffs.npy", diffs)
     X,Y = diffs.T
-    w = np.argmin(Y)
+    w = np.argmax(Y)
     return X[w]
 
