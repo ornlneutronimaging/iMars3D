81511fcb64c549a0e41b38866d469d3babae75ff
Jiao Lin <linjiao@ornl.gov>
2016-07-18 16:57:41 -0400

    Refs #48. added .tilt.find_rot_center

---
+++ python/imars3d/tilt/find_rot_center.py
@@ -0,0 +1,17 @@
+
+import os, numpy as np
+
+def find(ct_series, workdir=None):
+    img = lambda angle: ct_series.getImage(angle)
+    from . import _find180DegImgPairs
+    from .use_centers import computeTilt
+    pairs = _find180DegImgPairs(ct_series.identifiers)
+    centers = []
+    for i, (a0, a180) in enumerate(pairs):
+        workdir1=os.path.join(workdir, "%s_vs_%s"%(a0, a180))
+        slope, intercept = computeTilt(img(a0), img(a180), workdir=workdir1)
+        center = intercept + slope * img(a0).data.shape[0]
+        print(i, center)
+        centers.append(center)
+        continue
+    return np.mean(centers)
