ec7b0c315398518c56507764b9164f35b485f2c9
Jiao Lin <linjiao@ornl.gov>
2018-01-10 10:48:38 -0500

    Refs #87. refactor travis config so we can move the longer CT recon tests to a cron job

---
+++ builders/travis/before_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check
+df -h
+free
+
+# get MC
+if [ ${TRAVIS_PYTHON_VERSION:0:1} == "2" ]; then
+    wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-Linux-x86_64.sh -O miniconda.sh;
+else
+    wget http://repo.continuum.io/miniconda/Miniconda3-3.5.5-Linux-x86_64.sh -O miniconda.sh;
+fi
+
+# install MC
+chmod +x miniconda.sh
+./miniconda.sh -b -p /home/travis/mc
+
