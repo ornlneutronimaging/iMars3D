66f20eccbda5d0d7eb9581a6ebae1a210022ab99
Jiao Lin <linjiao@ornl.gov>
2018-01-10 14:08:50 -0500

    Refs #93. minor: echo from script.sh

--- builders/travis/script.sh
+++ builders/travis/script.sh
@@ -8,9 +8,13 @@ python tests/getdata.py
 python tests/imars3d/signon.py
 
 if [ ${TRAVIS_EVENT_TYPE} == "cron" ]; then
+    echo "* Running CT tests"
     travis_wait 60 python tests/workflows/recon/test_CT_travisCI.py test
     travis_wait 60 python tests/workflows/recon/test_CT_travisCI.py test2
+    echo "  - Done."
 else
+    echo "* Running unittests"
     py.test
+    echo "  - Done."
 fi
 
