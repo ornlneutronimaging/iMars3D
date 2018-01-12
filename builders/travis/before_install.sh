59177218afc5a717c5db6fb5dad800133904ae55
Jiao Lin <linjiao@ornl.gov>
2018-01-12 13:43:22 -0500

    Refs #91. remove miniconda before install

--- builders/travis/before_install.sh
+++ builders/travis/before_install.sh
@@ -10,7 +10,8 @@ free
 #    echo "Use cache"
 #else
     # get MC
-    if [ ${TRAVIS_PYTHON_VERSION:0:1} == "2" ]; then
+rm -rf /home/travis/mc
+if [ ${TRAVIS_PYTHON_VERSION:0:1} == "2" ]; then
 	wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-Linux-x86_64.sh -O miniconda.sh;
     else
 	wget http://repo.continuum.io/miniconda/Miniconda3-3.5.5-Linux-x86_64.sh -O miniconda.sh;
