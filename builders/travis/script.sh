d592060109b4e86417a6a68337b277c83d69d8b3
Jiao Lin <linjiao@ornl.gov>
2018-01-12 15:00:09 -0500

    Refs #91. cron: run one workflow test for now

--- builders/travis/script.sh
+++ builders/travis/script.sh
@@ -10,8 +10,8 @@ python tests/imars3d/signon.py
 if [ ${TRAVIS_EVENT_TYPE} == "cron" ]; then
     echo "* Running CT workflow tests"
     python tests/workflows/recon/test_CT_travisCI.py test
-    rm -rf work out
-    python tests/workflows/recon/test_CT_travisCI.py test2
+    # rm -rf work out
+    # python tests/workflows/recon/test_CT_travisCI.py test2
     echo "  - Done."
 else
     echo "* Running unittests"
