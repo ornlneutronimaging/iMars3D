b777ce187d612b8857a2d88dd9d431b79186b71b
Jiao Lin <linjiao@ornl.gov>
2018-01-12 14:03:32 -0500

    Refs #91. cached established. now back to normal

--- builders/travis/before_install.sh
+++ builders/travis/before_install.sh
@@ -6,12 +6,11 @@ set -e
 df -h
 free
 
-#if [ -d /home/travis/mc ] ; then
-#    echo "Use cache"
-#else
+if [ -d /home/travis/mc ] ; then
+    echo "Use cache"
+else
     # get MC
-rm -rf /home/travis/mc
-if [ ${TRAVIS_PYTHON_VERSION:0:1} == "2" ]; then
+    if [ ${TRAVIS_PYTHON_VERSION:0:1} == "2" ]; then
 	wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-Linux-x86_64.sh -O miniconda.sh;
     else
 	wget http://repo.continuum.io/miniconda/Miniconda3-3.5.5-Linux-x86_64.sh -O miniconda.sh;
@@ -20,4 +19,4 @@ if [ ${TRAVIS_PYTHON_VERSION:0:1} == "2" ]; then
     # install MC
     chmod +x miniconda.sh
     ./miniconda.sh -b -p /home/travis/mc
-#fi
+fi
