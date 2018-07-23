6efb8a730183c8facb8e336db42cdd23924202b3
Jiao Lin <linjiao@ornl.gov>
2018-07-22 23:15:12 -0400

    Refs #107. updated autocrop to support images with black borders

--- python/imars3d/autocrop.py
+++ python/imars3d/autocrop.py
@@ -9,16 +9,32 @@ def calculateCropWindow(series):
     save(ave, "estimate-ave.tiff")
     # smooth it
     from scipy import ndimage 
-    sm = ndimage.median_filter(ave, 9)
+    sm = ndimage.median_filter(ave, 9) # 21)
     save(sm, "sm-estimate-ave.tiff")
-    # find foreground rectangle
-    Y, X = np.where(sm < 0.8)
+    # make sure all numbers are smaller than 1.
+    max = np.max(sm);  sm/=max
+    # get background intensities
+    left = np.median(sm[:, :10])
+    top = np.median(sm[:10, :])
+    right = np.median(sm[:, -10:])
+    bottom = np.median(sm[-10:, :])
+    #
+    background_int = np.min([left, top, right, bottom])
+    # if background is dark
+    if background_int < .05:
+        Y1, X1 = np.where(sm>0.1)
+        xmin = np.min(X1); xmax = np.max(X1)
+        ymin = np.min(Y1); ymax = np.max(Y1)
+        return xmin, xmax, ymin, ymax
+    # find rectangle for real data
+    Y, X = np.where(sm < background_int*0.95)
     ymax = Y.max(); ymin = Y.min()
     xmax = X.max(); xmin = X.min()
     # expand it a bit
     width = xmax - xmin; height = ymax - ymin
-    xmin -= width/6.; xmax += width/6.
-    ymin -= height/6.; ymax += height/6.
+    expand_ratio = .15
+    xmin -= width*expand_ratio; xmax += width*expand_ratio
+    ymin -= height*expand_ratio; ymax += height*expand_ratio
     HEIGHT, WIDTH = ave.shape
     if xmin < 0: xmin = 0
     if xmax > WIDTH-1: xmax =WIDTH-1
