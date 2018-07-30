e5decd6c24f1272251d15867142b919ebd75cdf6
Jiao <jiao.lin@gmail.com>
2018-07-29 21:00:54 -0400

    continue debugging: force numpy version to 1.14

--- builders/travis/install.sh
+++ builders/travis/install.sh
@@ -13,8 +13,7 @@ else
     conda config --add channels conda-forge
     conda create -n testenv python=$TRAVIS_PYTHON_VERSION
     conda install -n testenv pip pytest
-    conda install -n testenv numpy scipy matplotlib
-    conda install -n testenv astropy mpich mpi4py
+    conda install -n testenv numpy=1.14 scipy matplotlib astropy mpich mpi4py
     conda install -n testenv pyyaml
     conda install -n testenv -c dgursoy tomopy=0.1.15
     # conda install -n testenv -c scikit-xray xraylib lmfit=0.8.3 netcdf4 # install from scikit-xray channel
