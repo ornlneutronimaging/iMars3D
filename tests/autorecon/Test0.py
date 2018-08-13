6d052cf6a9153d208ed7232cea7cb7f7ec8c67e3
Jiao Lin <jiao.lin@gmail.com>
2018-08-13 11:23:23 -0400

    Refs #109. rename tests in tests/autorecon so that they are not picked up by travis CI

--- tests/autorecon/Test0.py
+++ tests/autorecon/Test0.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+"""Test basic functionality:
+
+* skip reconstruction when CT is not done
+* strart reconstruction when working on last CT file
+"""
+
 import os, glob
 from imars3d.CT_from_TIFF_metadata import autoreduce
 
@@ -13,5 +19,5 @@ files = glob.glob(os.path.join(dir, '*.tiff'))
 for f in files:
     autoreduce(f, local_disk_partition='/tmp', parallel_nodes=20)
 
-ct_file_path = "/HFIR/CG1D/IPTS-21115/raw/ct_scans/July26_2018/20180727_Huggies_bottom_0015_359_910_2322.tiff"
-autoreduce(ct_file_path, local_disk_partition='/tmp', parallel_nodes=20)
+# ct_file_path = "/HFIR/CG1D/IPTS-21115/raw/ct_scans/July26_2018/20180727_Huggies_bottom_0015_359_910_2322.tiff"
+# autoreduce(ct_file_path, local_disk_partition='/tmp', parallel_nodes=20)
