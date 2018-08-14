6eab9e3e59efb3a84b0c49890a5f1a280ec60f67
Jiao Lin <jiao.lin@gmail.com>
2018-08-14 09:54:18 -0400

    Refs #111. Test_diapers: customize outdir and clean_intermediate_files

--- tests/autorecon/Test_diapers.py
+++ tests/autorecon/Test_diapers.py
@@ -5,4 +5,9 @@ from imars3d.CT_from_TIFF_metadata import autoreduce
 
 ct_file_path = "/HFIR/CG1D/IPTS-21115/raw/ct_scans/July26_2018/20180727_Huggies_bottom_0015_359_910_2322.tiff"
 # autoreduce(ct_file_path, local_disk_partition='/SNSlocal2', parallel_nodes=20, crop_window=(200, 0, 1447, 2047))
-autoreduce(ct_file_path, local_disk_partition='/SNSlocal2', parallel_nodes=20)
+autoreduce(
+    ct_file_path,
+    local_disk_partition='/SNSlocal2', 
+    clean_intermediate_files='archive',
+    outdir='/HFIR/CG1D/IPTS-21115/shared/autoreduce/CT-group-2495.test',
+    parallel_nodes=20)
