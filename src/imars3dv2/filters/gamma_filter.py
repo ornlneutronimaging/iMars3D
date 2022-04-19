cd9cdcf90fdc45c7b53fa9343e66e4340f26e602
Chen Zhang <zhangc@ornl.gov>
2022-04-19 15:23:53 -0400

    add unit test for gamma_filter

---
+++ src/imars3dv2/filters/gamma_filter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def gamma_filter():
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ tests/unit/filters/test_gamma_filter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import numpy as np
+import pytest
+import skimage
+from imars3dv2.filters.gamma_filter import gamma_filter
+
+
+def test_gamma_filter():
+    """ """
+    # constants
+    np.random.seed(7)
+    # input image
+    imgs_reference = skimage.data.brain()[:2, :, :]
+    imgs_with_noise = np.array(imgs_reference)
+    # synthetic gamma pixel location
+    NOISE_LOC = (
+        (174, 134),  # feature, high intensity
+        (103, 136),  # feature, low intensity
+        (177, 240),  # bg, weak intensity
+        (217, 131),  # bg, no intensity
+    )
+    #
+    saturation_intensity = np.iinfo(imgs_with_noise.dtype).max
+    for i, j in NOISE_LOC:
+        imgs_with_noise[j, i] = saturation_intensity - np.random.randint(0, 4)
+    # call gamma_filter
+    imgs_filtered = gamma_filter(imgs_with_noise)
+    # verify results
+    for i, j in NOISE_LOC:
+        # all the artificial gamma saturated pixels should be replaced
+        # with meaningful values (i.e. not saturated)
+        assert imgs_filtered[j, i] < saturation_intensity - 4
+
+
+if __name__ == "__main__":
+    pytest.main("( __file__")
