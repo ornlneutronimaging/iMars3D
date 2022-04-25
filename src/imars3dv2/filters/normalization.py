ec8d0dbe3e1ffa0931f4a229fde7604314a2a215
Chen Zhang <zhangc@ornl.gov>
2022-04-25 15:54:50 -0400

    follow tomopy naming convention

--- src/imars3dv2/filters/normalization.py
+++ src/imars3dv2/filters/normalization.py
@@ -5,18 +5,18 @@ import tomopy
 
 def normalization(
     arrays: np.ndarray,
-    whites: np.ndarray,
+    flats: np.ndarray,
     darks: np.ndarray,
     cut_off: float = -1.0,
     ncore: int = -1,
 ) -> np.ndarray:
     """
-    Normalize the input array(s) by subtracting the dark field and dividing by the adjusted white field.
+    Normalize the input array(s) by subtracting the dark field and dividing by the adjusted flat field.
 
     Parameters
     ----------
     @param arrays: 3D array of images, the first dimension is the rotation angle omega.
-    @param whites: 3D array of white field images (aka flat field, open beam), axis=0 is the image number axis.
+    @param flats: 3D array of flat field images (aka flat field, open beam), axis=0 is the image number axis.
     @param darks: 3D array of dark field images, axis=0 is the image number axis.
     @param cut_off: Permitted maximum value for the normalized data, originated from tomopy, negative values means no cutoff.
     @param ncore: number of cores to use for parallel processing, default is -1, which means using all available cores.
@@ -28,15 +28,15 @@ def normalization(
     # parse input (mostly for Tomopy)
     cut_off = None if cut_off < 0 else float(cut_off)
     ncore = None if ncore == -1 else int(ncore)
-    # use median filter to remove outliers from whites and darks
+    # use median filter to remove outliers from flats and darks
     # NOTE: this will remove the random noises coming from the environment.
-    whites = np.median(whites, axis=0)
+    flats = np.median(flats, axis=0)
     darks = np.median(darks, axis=0)
     # apply normalization
     # NOTE:
-    # For pixels where dark > white, tomopy replace the value with 1e-6 on-the-fly
+    # For pixels where dark > flat, tomopy replace the value with 1e-6 on-the-fly
     # see https://github.com/tomopy/tomopy/blob/master/source/tomopy/prep/normalize.py#L135
     #
-    arrays_normalized = tomopy.normalize(arrays, whites, darks, cutoff=cut_off, ncore=ncore)
+    arrays_normalized = tomopy.normalize(arrays, flats, darks, cutoff=cut_off, ncore=ncore)
     # return
     return arrays_normalized
--- tests/unit/filters/test_normalization.py
+++ tests/unit/filters/test_normalization.py
@@ -36,7 +36,7 @@ def generate_fake_darkfield(
     return dark
 
 
-def generate_fake_whitefield(
+def generate_fake_flatfield(
     img: np.ndarray,  # reference image
     beam_low: int = 15_000,  # incident beam intensity lower bound (counts)
     beam_high: int = 35_000,  # incident beam intensity upper bound (counts)
@@ -45,7 +45,7 @@ def generate_fake_whitefield(
 ):
     n_row, n_col = img.shape
 
-    fake_white = np.random.randint(
+    fake_flat = np.random.randint(
         low=beam_low,
         high=beam_high,
         size=(n_row, n_col),
@@ -55,18 +55,18 @@ def generate_fake_whitefield(
     loc_pk1 = (int(n_row / 2), int(n_col / 4))
     loc_pk2 = (int(n_row / 2), int(n_col / 4) * 3)
 
-    fake_white[loc_pk1[0] - beam_height : loc_pk1[0] + beam_height, loc_pk1[1]] = np.iinfo(img.dtype).max
-    fake_white[loc_pk2[0] - beam_height : loc_pk2[0] + beam_height, loc_pk2[1]] = np.iinfo(img.dtype).max
+    fake_flat[loc_pk1[0] - beam_height : loc_pk1[0] + beam_height, loc_pk1[1]] = np.iinfo(img.dtype).max
+    fake_flat[loc_pk2[0] - beam_height : loc_pk2[0] + beam_height, loc_pk2[1]] = np.iinfo(img.dtype).max
 
     # mimic the beam profile from HIFR
     for _ in range(gaussian_pass_num):
-        fake_white = gaussian_filter(fake_white, sigma=beam_height / 2)
+        fake_flat = gaussian_filter(fake_flat, sigma=beam_height / 2)
 
-    return fake_white
+    return fake_flat
 
 
 def prepare_synthetic_data():
-    """prepare synthetic whites, darks, raw, and reference proj"""
+    """prepare synthetic flats, darks, raw, and reference proj"""
     np.random.seed(7)
     # input image (reference)
     img = skimage.data.brain()[7, :, :]
@@ -77,24 +77,24 @@ def prepare_synthetic_data():
         high=img.shape[0],
         size=(num_bad_pixels, 2),
     )
-    # generage fake darks and whites
+    # generage fake darks and flats
     num_dark = 10
-    num_white = 10
+    num_flat = 10
     darks = np.array([generate_fake_darkfield(img, bad_pixels) for _ in range(num_dark)]).reshape(num_dark, *img.shape)
-    whites = np.array([generate_fake_whitefield(img) for _ in range(num_white)]).reshape(num_white, *img.shape)
+    flats = np.array([generate_fake_flatfield(img) for _ in range(num_flat)]).reshape(num_flat, *img.shape)
     # make synthetic projections (normalized) and raw image (non-normalized)
     proj = 1.0 - img / np.iinfo(img.dtype).max
     dark = np.median(darks, axis=0)
-    white = np.median(whites, axis=0)
-    raw = proj * (white - dark) + dark
-    return raw, darks, whites, proj
+    flat = np.median(flats, axis=0)
+    raw = proj * (flat - dark) + dark
+    return raw, darks, flats, proj
 
 
 def test_normalization_standard():
-    """standard normalization routine with reasonable dark and white."""
-    raw, darks, whites, proj = prepare_synthetic_data()
+    """standard normalization routine with reasonable dark and flat."""
+    raw, darks, flats, proj = prepare_synthetic_data()
     # process with normalization
-    proj_imars3d = normalization(raw, whites, darks)
+    proj_imars3d = normalization(raw, flats, darks)
     # compare
     diff = np.absolute(proj_imars3d - proj).sum() / np.prod(proj.shape)
     assert diff < 0.01
@@ -102,15 +102,15 @@ def test_normalization_standard():
 
 def test_normalization_bright_dark():
     """normalization routine where dark contains pixels that has higher counts
-    than white due to hardware defect.
+    than flat due to hardware defect.
     """
-    raw, darks, whites, proj = prepare_synthetic_data()
-    # randomly select one pixel in dark to have higher counts than white
+    raw, darks, flats, proj = prepare_synthetic_data()
+    # randomly select one pixel in dark to have higher counts than flat
     for i in range(darks.shape[0]):
         ix, iy = np.random.randint(low=0, high=darks.shape[1], size=2)
-        darks[i, ix, iy] += whites[i, ix, iy]
+        darks[i, ix, iy] += flats[i, ix, iy]
     # process with normalization
-    proj_imars3d = normalization(raw, whites, darks)
+    proj_imars3d = normalization(raw, flats, darks)
     # compare
     diff = np.absolute(proj_imars3d - proj).sum() / np.prod(proj.shape)
     assert diff < 0.01
