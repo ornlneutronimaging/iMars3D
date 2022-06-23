260f11542a8dec9211a5119f0858ccab0ac32000
Chen Zhang <zhangc@ornl.gov>
2022-06-23 10:05:30 -0400

    add unit test for ring removal

---
+++ tests/unit/filters/test_ring_removal.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import numpy as np
+import pytest
+import tomopy
+from imars3dv2.filters.ring_removal import remove_ring_artifact
+from imars3dv2.filters.ring_removal import remove_ring_artifact_Ketcham
+
+
+def get_synthetic_stack(N_omega: int = 181) -> np.ndarray:
+    omegas = np.linspace(0, np.pi * 2, N_omega)
+    shepp3d = tomopy.misc.phantom.shepp3d(size=200)
+    # use emission type radiograph to skip the -log step
+    projs = tomopy.sim.project.project(shepp3d, omegas, emission=True)
+    return projs
+
+
+def test_remove_ring_artifact():
+    # get synthetic stack
+    tomo_stack = get_synthetic_stack()
+    # add ring artifact
+    # randomly adding 0-4 rings to each sinogram
+    tomo_with_ring = np.array(tomo_stack)
+    for i in range(tomo_with_ring.shape[1]):
+        n_rings = np.random.randint(0, 4)
+        for _ in range(n_rings):
+            col = np.random.randint(80, 200)
+            factor = 1 + (np.random.random() - 0.5) / 10  # [0.9,1.1]
+            tomo_with_ring[:, i, col] *= factor
+    # perform correction
+    tomo_corr = remove_ring_artifact(tomo_with_ring)
+    # verify
+    err_no_corr = np.linalg.norm(tomo_with_ring - tomo_stack) / tomo_stack.size
+    err_corr = np.linalg.norm(tomo_corr - tomo_stack) / tomo_stack.size
+    assert err_corr < err_no_corr
+
+
+def test_remove_ring_artifact_Ketcham():
+    # get synthetic stack
+    tomo_stack = get_synthetic_stack()
+    # add ring artifact
+    tomo_with_ring = np.array(tomo_stack)
+    tomo_with_ring[:, 100, 80] *= 1.07
+    tomo_with_ring[:, 100, 100] *= 1.05
+    tomo_with_ring[:, 100, 150] *= 0.92
+    tomo_with_ring[:, 100, 200] *= 0.92
+    # select one sinogram for testing
+    sino_ref = tomo_stack[:, 100, :]
+    sino = tomo_with_ring[:, 100, :]
+    sino_corrected = remove_ring_artifact_Ketcham(sino, kernel_size=5)
+    # verify
+    #   The correction won't be perfect, but it should be better than the
+    #   non-corrected one.
+    err_no_correction = np.linalg.norm(sino - sino_ref) / sino.size
+    err_correction = np.linalg.norm(sino_corrected - sino_ref) / sino.size
+    assert err_correction < err_no_correction
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
