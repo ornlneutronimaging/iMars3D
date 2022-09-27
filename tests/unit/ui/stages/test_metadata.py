4d1d030b52c92a6b405ff43b3ff497a91bc34da9
Chen Zhang <zhangc@ornl.gov>
2022-09-27 15:16:01 -0400

    add new metadata ui

--- tests/unit/ui/stages/test_metadata.py
+++ tests/unit/ui/stages/test_metadata.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+import panel as pn
+from panel.io.server import serve
+from playwright.sync_api import Page, expect
+from imars3d.ui.stages.metadata import MetaData
+
+
+pn.extension("jsoneditor")
+
+def test_metadata():
+    # start the app
+    app = pn.panel(MetaData().panel)
+    serve(app, port=1960, threaded=True, show=True)
+    pass
+
+
+if __name__ == "__main__":
+    test_metadata()
