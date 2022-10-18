4a1ada58386bd44303e2a8305ffa53c5c5733511
Pete Peterson <petersonpf@ornl.gov>
2022-10-18 15:15:53 -0400

    Convert to being a ParamerizedFunction

--- src/imars3d/backend/corrections/ring_removal.py
+++ src/imars3d/backend/corrections/ring_removal.py
@@ -13,13 +13,7 @@ logger = param.get_logger(__name__)
 logger.name = __name__
 
 
-def remove_ring_artifact(
-    arrays: np.ndarray,
-    kernel_size: int = 5,
-    sub_division: int = 10,
-    correction_range: tuple = (0.9, 1.1),
-    ncore: int = -1,
-) -> np.ndarray:
+class remove_ring_artifact(param.ParameterizedFunction):
     """Remove ring artifact from radiograph stack using Ketcham method.
 
     ref: `10.1117/12.680939 <https://doi.org/10.1117/12.680939>`_
@@ -27,56 +21,103 @@ def remove_ring_artifact(
     Parameters
     ----------
     arrays:
-        Input radiograph stack.
+       Input radiograph stack.
     kernel_size:
-        The size of the kernel (moving window) during local smoothing with median filter.
+        The size of the kernel (moving window) during local smoothing with median filter (default=5)
     sub_division:
-        Sub-dividing the sinogram into subsections (along rotation angle axis).
+        Sub-dividing the sinogram into subsections (along rotation angle axis) (default=10)
     correction_range:
-        Multiplicative correction factor is capped within given range.
-    ncore:
-        Number of cores to use for parallel processing.
+        Multiplicative correction factor is capped within given range (default=(0.9, 1.1))
+    max_workers:
+        Number of cores to use for parallel processing (default=-1)
 
     Returns
     -------
-        Radiograph stack with ring artifact removed.
+         Radiograph stack with ring artifact removed.
     """
-    logger.info("Executing Filter: Remove Ring Artifact (tomopy)")
-    # sanity check
-    if arrays.ndim != 3:
-        raise ValueError("This correction can only be used for a stack, i.e. a 3D image.")
-    # NOTE:
-    # additional work is needed to avoid duplicating arrays in memory
-    ncore = mproc.mp.cpu_count() if ncore == -1 else int(ncore)
-    # use shared memory to reduce memory footprint
-    with SharedMemoryManager() as smm:
-        # create the shared memory
-        shm = smm.SharedMemory(arrays.nbytes)
-        # create a numpy array point to the shared memory
-        shm_arrays = np.ndarray(
-            arrays.shape,
-            dtype=arrays.dtype,
-            buffer=shm.buf,
+
+    arrays = param.Array(doc="3D array of images, the first dimension is the rotation angle omega.")
+    kernel_size = param.Integer(
+        default=5, doc="The size of the kernel (moving window) during local smoothing with median filter"
+    )
+    sub_division = param.Integer(
+        default=10, doc="Sub-dividing the sinogram into subsections (along rotation angle axis)"
+    )
+    correction_range = param.Tuple(
+        default=(0.9, 1.1), doc="Multiplicative correction factor is capped within given range."
+    )
+    max_workers = param.Integer(
+        default=-1,
+        bounds=(-1, None),
+        doc="Maximum number of processes to use for parallel median filtering, default is -1, which means using all available cores.",
+    )
+
+    def __call__(self, **params):
+        logger.info("Executing Filter: Remove Ring Artifact (tomopy)")
+
+        _ = self.instance(**params)
+        params = param.ParamOverrides(self, params)
+        logger.debug(
+            "remove_ring_artifact( CT",
+            params.kernel_size,
+            params.sub_division,
+            params.correction_range,
+            params.max_workers,
+            ")",
         )
-        # copy the data to the shared memory
-        np.copyto(shm_arrays, arrays)
-        # invoke mp via tqdm wrapper
-        rst = process_map(
-            partial(
-                remove_ring_artifact_Ketcham,
-                kernel_size=kernel_size,
-                sub_division=sub_division,
-                correction_range=correction_range,
-            ),
-            [shm_arrays[:, sino_idx, :] for sino_idx in range(shm_arrays.shape[1])],
-            max_workers=ncore,
-            desc="Removing ring artifact",
+        val = self._remove_ring_artifact(
+            params.arrays,
+            int(params.kernel_size),
+            int(params.sub_division),
+            params.correction_range,
+            int(params.max_workers),
         )
-    rst = np.array(rst)
-    for i in range(arrays.shape[1]):
-        arrays[:, i, :] = rst[i]
-    logger.info("FINISHED Executing Filter: Remove Ring Artifact (tomopy)")
-    return arrays
+
+        logger.info("FINISHED Executing Filter: Remove Ring Artifact (tomopy)")
+        return val
+
+    def _remove_ring_artifact(
+        self,
+        arrays: np.ndarray,
+        kernel_size: int,
+        sub_division: int,
+        correction_range: tuple,
+        max_workers: int,
+    ) -> np.ndarray:
+        # sanity check
+        if arrays.ndim != 3:
+            raise ValueError("This correction can only be used for a stack, i.e. a 3D image.")
+        # NOTE:
+        # additional work is needed to avoid duplicating arrays in memory
+        max_workers = mproc.mp.cpu_count() if max_workers == -1 else max_workers
+        # use shared memory to reduce memory footprint
+        with SharedMemoryManager() as smm:
+            # create the shared memory
+            shm = smm.SharedMemory(arrays.nbytes)
+            # create a numpy array point to the shared memory
+            shm_arrays = np.ndarray(
+                arrays.shape,
+                dtype=arrays.dtype,
+                buffer=shm.buf,
+            )
+            # copy the data to the shared memory
+            np.copyto(shm_arrays, arrays)
+            # invoke mp via tqdm wrapper
+            rst = process_map(
+                partial(
+                    remove_ring_artifact_Ketcham,
+                    kernel_size=kernel_size,
+                    sub_division=sub_division,
+                    correction_range=correction_range,
+                ),
+                [shm_arrays[:, sino_idx, :] for sino_idx in range(shm_arrays.shape[1])],
+                max_workers=max_workers,
+                desc="Removing ring artifact",
+            )
+            rst = np.array(rst)
+        for i in range(arrays.shape[1]):
+            arrays[:, i, :] = rst[i]
+        return arrays
 
 
 def remove_ring_artifact_Ketcham(
