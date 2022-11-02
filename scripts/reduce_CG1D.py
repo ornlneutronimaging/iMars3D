78739b73ec632b20a038445a7996c17c16b4008d
Pete Peterson <petersonpf@ornl.gov>
2022-11-02 11:16:34 -0400

    Change from print to logging

--- scripts/reduce_CG1D.py
+++ scripts/reduce_CG1D.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 from pathlib import Path
 import sys
+import logging
 
 
 # declare the conda environment for this to run in
 CONDA_ENV = "imars3d-dev"
 
+logger = logging.getLogger("reduce_CG1D")
+
 
 def _validate_inputs(inputfile: Path, outputdir: Path) -> int:
     input_checking = 0
     if not inputfile.is_file():
-        print(f"{inputfile} is not a file")
+        logger.error(f"'{inputfile}' is not a file")
         input_checking += 1
     if not outputdir.is_dir():
-        print(f"{outputdir} is not a directory")
+        logger.error(f"'{outputdir}' is not a directory")
         input_checking += 1
 
     return input_checking
@@ -37,8 +40,8 @@ def main(inputfile: str, outputdir: str) -> int:
     # convert the parameters to make the rest easier
     inputfile = Path(inputfile)
     outputdir = Path(outputdir)
-    print("INPUT:", inputfile)  # TODO remove
-    print("OUTPUT:", outputdir)  # TODO remove
+    logging.info("INPUT:", inputfile)  # TODO remove
+    logging.info("OUTPUT:", outputdir)  # TODO remove
 
     # verify the inputs are sensible
     input_checking = _validate_inputs(inputfile, outputdir)
