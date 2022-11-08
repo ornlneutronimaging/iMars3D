e9f3b00a603825d01e08a1c1c4dee6600b0b5aba
Chen Zhang <zhangc@ornl.gov>
2022-11-08 14:51:18 -0500

    add skeletons for autoredux

--- src/imars3d/backend/__init__.py
+++ src/imars3d/backend/__init__.py
@@ -1,1 +1,4 @@
 """iMars3D's backend packages: the core of the application."""
+from .autoredux import auto_reduction_ready
+from .autoredux import load_template_config
+from .autoredux import extract_info_from_path
---
+++ tests/unit/backend/test_autoredux.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import pytest
+from imars3d.backend import auto_reduction_ready
+from imars3d.backend import load_template_config
+from imars3d.backend import extract_info_from_path
+
+
+def test_auto_reduction_ready():
+    # NOTE: update the test when implement the function
+    with pytest.raises(NotImplementedError):
+        auto_reduction_ready(data_file="test")
+
+
+def test_load_template_config():
+    # NOTE: update the test when implement the function
+    with pytest.raises(NotImplementedError):
+        load_template_config()
+
+
+def test_extract_info_from_path():
+    # NOTE: update the test when implement the function
+    with pytest.raises(NotImplementedError):
+        extract_info_from_path(data_file="test")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
