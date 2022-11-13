2629569db4d3961ddcff8fb53b4df56aa83b480b
Jose Borreguero <borreguero@gmail.com>
2022-11-13 09:51:24 -0500

    function to find the template configuration
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- tests/integration/autoreduction/test_no_template.py
+++ tests/integration/autoreduction/test_no_template.py
@@ -1,5 +1,5 @@
 # package imports
-from reduce_CG1D import ERROR_GENERAL
+from reduce_CG1D import ERROR_GENERAL, logger
 from reduce_CG1D import main as main_CG1D
 
 # third-party imports
@@ -12,8 +12,7 @@ from pathlib import Path
 @pytest.mark.datarepo
 def test_no_template(IRON_MAN_DIR: Path, tmp_path: Path):
     last_tiff = IRON_MAN_DIR / "20191030_ironman_small_0070_360_760_0624.tiff"
-    exit_code = main_CG1D(last_tiff, tmp_path)
-    assert exit_code == ERROR_GENERAL
+    assert main_CG1D(last_tiff, tmp_path) == ERROR_GENERAL
     # fetch string in the logs
 
 
