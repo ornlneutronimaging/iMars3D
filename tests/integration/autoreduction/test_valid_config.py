fca275453f28a620b77f8990c9257d2433651c1f
Jose Borreguero <borreguero@gmail.com>
2022-11-18 09:07:38 -0500

    changes after working substitute_template in branch 'next'
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- tests/integration/autoreduction/test_valid_config.py
+++ tests/integration/autoreduction/test_valid_config.py
@@ -1,4 +1,5 @@
 # package imports
+from imars3d.backend.autoredux import extract_info_from_path, substitute_template
 from reduce_CG1D import main as main_CG1D
 from reduce_CG1D import WORKFLOW_SUCCESS
 
@@ -8,7 +9,7 @@ import numpy as np
 import pytest
 
 # standard imports
-import json
+import os
 from pathlib import Path
 import re
 from unittest import mock
@@ -16,48 +17,56 @@ from unittest.mock import MagicMock
 
 
 @pytest.fixture(scope="module")
-def load_template(IRON_MAN_DIR):
-    r"""Load the template and set paths for radiographs, open-beam, and dark-current"""
+def tweak_extract_info_from_path():
+    r"""Call extract_info after amending the input_tiff Path
+    The amended path should start as /HFIR/CG1D/ so that
+    extract_info_from_path() can work properly
+    """
 
-    def _load_template(config_path: Path):
-        with open(config_path, "r") as template_config:
-            config = json.load(template_config)
-        # replace relative with absolute paths
-        inputs = config["tasks"][0]["inputs"]
-        inputs["ct_dir"] = str(IRON_MAN_DIR)
-        inputs["ob_dir"] = str(IRON_MAN_DIR.parent.parent / "ob" / "Oct29_2019")
-        inputs["dc_dir"] = str(IRON_MAN_DIR.parent.parent / "df" / "Oct29_2019")
+    def _tweak_extract_info_from_path(path_to_tiff: Path) -> dict:
+        items = Path(path_to_tiff).parts
+        new_path = Path("/" + os.path.sep.join(items[items.index("HFIR") :]))
+        config = extract_info_from_path(new_path)
         return config
 
-    return _load_template
+    return _tweak_extract_info_from_path
 
 
-# TODO: remove the mocks as the parts of reduce_CG1D become completed
+@pytest.fixture(scope="module")
+def tweak_substitute_template(DATA_DIR):
+    r"""Amend the path to the data directories because they don't conform to the convention"""
+
+    def _tweak_substitute_template(config: dict, values: dict) -> dict:
+        updated = substitute_template(config, values)
+        assert updated["tasks"][0]["name"] == "load_data"
+        inputs = updated["tasks"][0]["inputs"]
+        path_to_raw = Path(inputs["ct_dir"]).parent.parent  # /HFIR/CG1D/IPTS-25777/raw
+        path_to_raw = path_to_raw.relative_to(DATA_DIR.anchor)  # transform to a relative path
+        inputs["ct_dir"] = str(DATA_DIR / path_to_raw / "ct_scans/iron_man")
+        inputs["ob_dir"] = str(DATA_DIR / path_to_raw / "ob/Oct29_2019")  # suffix should also be "iron_man"
+        inputs["dc_dir"] = str(DATA_DIR / path_to_raw / "df/Oct29_2019")
+        return updated
+
+    return _tweak_substitute_template
+
+
 @pytest.mark.datarepo
+@mock.patch("reduce_CG1D.substitute_template")
 @mock.patch("reduce_CG1D.extract_info_from_path")
-@mock.patch("reduce_CG1D.load_template_config")
-@mock.patch("reduce_CG1D._find_template_config")
 def test_valid_config(
-    mock__find_template_config: MagicMock,
-    mock_load_template_config: MagicMock,
     mock_extract_info_from_path: MagicMock,
-    load_template,
+    mock_substitute_template: MagicMock,
+    tweak_extract_info_from_path,
+    tweak_substitute_template,
     DATA_DIR: Path,
     JSON_DIR: Path,
     IRON_MAN_DIR: Path,
     tmp_path: Path,
     caplog,
 ):
-    mock__find_template_config.return_value = JSON_DIR / "good_non_interactive_full.json"
-    mock_load_template_config.return_value = load_template(JSON_DIR / "good_non_interactive_full.json")
-    mock_extract_info_from_path.return_value = {
-        "ipts": "IPTS-25777",
-        "name": "reduce CG1D",
-        "workingdir": str(tmp_path),
-        "outputdir": str(tmp_path),
-    }
     last_tiff = IRON_MAN_DIR / "20191030_ironman_small_0070_360_760_0624.tiff"
-
+    mock_extract_info_from_path.return_value = tweak_extract_info_from_path(last_tiff)
+    mock_substitute_template.side_effect = tweak_substitute_template
     # Check for autoreduction success
     assert main_CG1D(last_tiff, tmp_path) == WORKFLOW_SUCCESS
 
