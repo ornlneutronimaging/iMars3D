a1ba89e8e742de4acf2e04c8921302ecad3bc215
Jose Borreguero <borreguero@gmail.com>
2022-11-21 21:38:37 -0500

    avoid having two tests writing to the same output directory
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,34 +20,6 @@ def IRON_MAN_DIR(DATA_DIR):
     return DATA_DIR / "HFIR/CG1D/IPTS-25777/raw/ct_scans/iron_man"
 
 
-@pytest.fixture(scope="module")
-def cleanfile():
-    """Fixture that deletes registered files when the .py file is finished. It
-    will cleanup on exception and will safely skip over files that do not
-    exist. Do not use this if you want the files to remain for a failing test.
-
-    Usage:
-
-    def test_something(cleanfile):
-        cleanfile('/some/file/the/test.creates')
-        # do stuff
-    """
-    filenames = []
-
-    def _cleanfile(filename):
-        filenames.append(Path(filename))
-        return filename
-
-    yield _cleanfile
-
-    for filename in filenames:
-        if filename.exists():
-            if filename.is_dir():
-                rmtree(filename)  # remove the directory and any files that are in it
-            else:
-                filename.unlink()  # remove the single file
-
-
 # NOTE: pytest fixtures tmp_path and tmp_path_factory are NOT deleting the temporary directory, hence this fixture
 @pytest.fixture(scope="function")
 def tmpdir():
